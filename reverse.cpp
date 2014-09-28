c0e404a9ef4d2bf721f6fa8e801d546b805aefe8
Abhinav Jhanwar <abhinav.jhanwar.august2@gmail.com>
2014-09-28 20:27:28 +0530

    Create reverse.cpp

---
+++ reverse.cpp
@@ -0,0 +1,38 @@
+#include<iostream.h>
+#include<conio.h>
+#include<stdio.h>
+#include<string.h>
+
+static void reversify(char * ar);
+
+void main()
+{
+clrscr();
+char ar[256];
+cout<<"--------------------------------REVERSIFIER-----------------------------\n\n\n";
+cout<<"Enter sentence- ";
+gets(ar);
+reversify(ar);
+getch();
+}
+
+static void reversify(char * ar)
+{
+int i=strlen(ar)-1,keep,end='\0';
+	while(i>=0)
+	{
+		while(i>=0 && ar[i]!=' ')
+			i--;
+			keep=i-1;
+			i++;
+		while(ar[i]!=end)
+		{
+			cout<<ar[i];
+			i++;
+		}
+		end=' ';
+		i=keep;
+		if(i>0)
+		cout<<" ";
+	}
+}
